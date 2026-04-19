811b115379b91e0008ead66c6c5a2e6f1dfd364f
Ramakrishna Mamidi <ramakrishnamamidi@gmail.com>
2026-04-19 17:41:39 +0530

    Update Snowflake setup.sql with correct AWS account and bucket

--- snowflake/setup.sql
+++ snowflake/setup.sql
@@ -18,8 +18,8 @@ CREATE STORAGE INTEGRATION s3_insurance_integration
   TYPE = EXTERNAL_STAGE
   STORAGE_PROVIDER = 'S3'
   ENABLED = TRUE
-  STORAGE_AWS_ROLE_ARN = 'arn:aws:iam::YOUR_ACCOUNT_ID:role/SnowflakeS3Role'
-  STORAGE_ALLOWED_LOCATIONS = ('s3://insurance-lakehouse-yourname/');
+   STORAGE_AWS_ROLE_ARN = 'arn:aws:iam::296588423667:role/SnowflakeS3Role'
+   STORAGE_ALLOWED_LOCATIONS = ('s3://insurance-lakehouse-demo-2026/');
 
 -- Get the Snowflake IAM values (add to your AWS role trust policy)
 DESC INTEGRATION s3_insurance_integration;
